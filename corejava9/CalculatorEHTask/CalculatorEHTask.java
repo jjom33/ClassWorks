f1d230ccd922751b3caad43f4586c6b42bc3f847
jjom33 <j.jiyeonjung33@gmail.com>
2023-06-26 15:54:16 -0400

    Calculator Task uploaded

---
+++ corejava9/CalculatorEHTask/CalculatorEHTask.java
@@ -0,0 +1,54 @@
+package corejava9;
+import java.util.InputMismatchException;
+import java.util.Scanner;;
+public class CalculatorEHTask {
+    CalculatorEHTask(){   
+    }
+
+    public void calculate(){
+        Scanner scanner = new Scanner(System.in);
+        String userInput="";
+        do{
+            if(!userInput.equals("exit")){
+                try{
+                    System.out.println("\nEnter 1: ");
+                    int num1 = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Enter 2: ");
+                    int num2 = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Enter operation (+, -, *, /, %): ");
+                    String operation = scanner.nextLine();
+
+                    double result=0;
+                    switch(operation){
+                        case "+":
+                            result = num1 + num2;
+                            break;
+                        case "-":
+                            result = num1- num2;
+                            break;
+                        case "*":
+                            result = num1*num2;
+                            break;
+                        case "/":
+                            result = num1/ num2;
+                            break;
+                        case "%":
+                            result = num1%num2;
+                            break;
+                        default:
+                            throw new InputMismatchException("There is no such operation");
+                    }
+                    System.out.println("calculation result: "+num1+" "+operation+" "+num2+" = "+result);
+                    System.out.println("\nContinue another calculation? (yes / exit)");
+                    userInput =scanner.nextLine();
+                } catch (ArithmeticException |ArrayIndexOutOfBoundsException|InputMismatchException |NumberFormatException e){
+                    System.out.println("!Exception caught: \n\t"+e+"\nPlease try again");
+                } 
+            }
+        }while(!userInput.equals("exit"));
+        scanner.close();
+    }
+        
+        
+     
+}
---
+++ corejava9/CalculatorEHTaskMain.java
@@ -0,0 +1,8 @@
+package corejava9;
+
+public class CalculatorEHTaskMain {
+    public static void main(String[] args){
+        CalculatorEHTask calculator = new CalculatorEHTask();
+        calculator.calculate(); 
+    }
+}
